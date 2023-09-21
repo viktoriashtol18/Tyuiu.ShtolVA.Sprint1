123886f1bbadb6760aa33309368202b23272a771
viktoriashtol18 <viktoriastol878@gmail.com>
2023-09-22 00:12:14 +0500

    Task2

--- Tyuiu.ShtolVA.Sprint1.Task2.V16.Test/DataServiceTest.cs
+++ Tyuiu.ShtolVA.Sprint1.Task2.V16.Test/DataServiceTest.cs
@@ -11,9 +11,9 @@ namespace Tyuiu.PaulikKV.Sprint1.Task2.V16.Test
         public void ValidExpression()
         {
             DataService ds = new DataService();
-            int x = 1;
+            int x = 2;
             var res = ds.CalculatePerimetrCircle(x);
-            Assert.AreEqual(6.28, res);
+            Assert.AreEqual(12.56, res);
 
         }
     }
